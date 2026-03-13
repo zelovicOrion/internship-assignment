1b3a5f61779d625953e4e10d29fed13ce16656ea
Luka Zelovic <luka.zelovic@mera.com>
2026-03-13 14:12:04 +0100

    Added project structure

---
+++ internship-assignment/src/main/java/com/temenos/internship/assignment/InternshipAssignmentApplication.java
@@ -0,0 +1,13 @@
+package com.temenos.internship.assignment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InternshipAssignmentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InternshipAssignmentApplication.class, args);
+	}
+
+}
---
+++ internship-assignment/src/test/java/com/temenos/internship/assignment/InternshipAssignmentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.temenos.internship.assignment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InternshipAssignmentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
